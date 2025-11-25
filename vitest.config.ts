71b3f2055270dc54e9e07b821f50bb9a6e898225
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2025-11-25 01:29:56 +0000

    allow ci to pass without test suite

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    passWithNoTests: true,
     exclude: [
       ...configDefaults.exclude,
       'tests/**/*.e2e.{ts,tsx}',
